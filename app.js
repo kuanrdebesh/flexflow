1b16bd37a3885565bbf4797aa6b8120a1d675c7f
kuanrdebesh <kuanr.debesh@gmail.com>
2026-03-21 19:47:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -143,6 +143,7 @@ var IMAGE_STRETCHES = {
   'post-wall-tricep-press' : 'images/post-wall-tricep-press.png',
   'Post-Workout' : 'images/Post-Workout.png',
   'act-neck-rotation' : 'images/act-neck-rotation.png',
+  'post-scm-str' : 'images/post-scm-stretch',
 };
 
 // ── MUSCLE GROUPS ─────────────────────────────────────
